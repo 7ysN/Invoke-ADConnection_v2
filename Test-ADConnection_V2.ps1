bb08611719add735f1ada5f81db245a236fda369
ysN <62604022+7ysN@users.noreply.github.com>
2023-10-15 18:46:27 +0300

    Update Test-ADConnection_V2.ps1

--- Test-ADConnection_V2.ps1
+++ Test-ADConnection_V2.ps1
@@ -1,4 +1,4 @@
-# Author: Yuval Saban
+
 # This Script helps to perform a connectivity check against the Active Directory LDAP service and Using Active Directory Module for creating the Users list.
 
 
